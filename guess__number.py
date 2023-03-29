1936b5cdc7c1665b6f0cb6299ff3f7091288ec02
Rafa3141 <rei42782@gmail.com>
2023-03-29 12:20:34 -0300

    Ainda aprimorando o código

--- guess__number.py
+++ guess__number.py
@@ -1,32 +1,37 @@
+#Aqui importamos as bibliotecas random(para gerar o número aleatório) e math(para as operações matemáticas):
 import random
 import math
+
 print("Algoritimo desenvolvido para a brincadeira adivinhe o número")
-lower = int(input("Digite o limite do menor número:- ")) 
+
+#Definindo as variáveis input dos limites entre o número:
+menor = int(input("Digite o limite do menor número:- "))  
+maior = int(input("Digite o limite do maior número:- ")) 
  
-upper = int(input("Digite o limite do maior número:- "))  
+ #Aqui utilizamos a função randint que é uma função em Python que gera um valor aleatório entre dois valores inteiros: 
+x = random.randint(menor, maior)
+
+print("\n\tVocê tem apenas ", round(math.log(maior - menor + 1, 2))," chances de acertar o inteiro!\n")
  
-x = random.randint(lower, upper)
-print("\n\tVocê tem apenas ", round(math.log(upper - lower + 1, 2))," chances de acertar o inteiro!\n")
  
- 
-count = 0
- 
-while count < math.log(upper - lower + 1, 2):
-    count += 1
+contador = 0
+
+while contador < math.log(maior - menor + 1, 2):
+    contador += 1
      
      
-    guess = int(input("Adivinhe o número:- ")) 
+    adivinhe = int(input("Adivinhe o número:- ")) 
      
     
-    if x == guess:  
-       print("Parabéns você adivinhou em ", count, " tentativas")
+    if x == adivinhe:  
+       print("Parabéns você adivinhou em ", contador, " tentativas")
        
        break
-    elif x > guess:
+    elif x > adivinhe:
        print("Número muito pequeno!")
-    elif x < guess:
+    elif x < adivinhe:
        print("Número muito grande!")
  
-if count >= math.log(upper - lower + 1, 2):
+if contador >= math.log(maior - menor + 1, 2):
    print("\nO número é %d"%x)
    print("\tMais sorte da próxima vez!")
